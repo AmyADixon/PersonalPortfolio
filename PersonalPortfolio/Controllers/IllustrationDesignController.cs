e9e2bda2e2ca38785a3891a444a8aaa1406fed9c
Amy Dixon <amysha.dixon@gmail.com>
2018-10-28 16:15:52 -0700

    Added ResumeController and IllustrationDesignController

---
+++ PersonalPortfolio/Controllers/IllustrationDesignController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalPortfolio.Controllers {
+    public class IllustrationDesignController : Controller {
+        // GET: IllustrationDesign
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
