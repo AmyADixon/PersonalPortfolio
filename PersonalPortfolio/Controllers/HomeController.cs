ac149a2e043e4ea5520ea93f42d5cbac331e896a
Amy Dixon <amysha.dixon@gmail.com>
2018-11-20 20:04:31 -0800

    Ceated a partial view, Main.cshtml, for the 'Home' tab so that page does not refresh when user selects it

--- PersonalPortfolio/Controllers/HomeController.cs
+++ PersonalPortfolio/Controllers/HomeController.cs
@@ -6,8 +6,12 @@ using System.Web.Mvc;
 
 namespace PersonalPortfolio.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() { // Always a full view, info will instead be injected into it
+        public ActionResult Index() { 
             return View();
+        }
+
+        public ActionResult Main() {
+            return PartialView();
         }
 
         public ActionResult About() {
